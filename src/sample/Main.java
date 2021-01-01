1e628eaeb3f215739a0027c75ae0f15e235709dd
Roman Verekh <vrbdsp@gmail.com>
2021-01-01 16:46:23 +0200

    Final commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,11 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setTitle("My Chat");
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("css/style.css");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
